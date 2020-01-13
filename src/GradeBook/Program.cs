ff6ef075113a4fe3d26b71760f5e5bd7ffc85b41
robert-cummins <robert.k.cummins@gmail.com>
2020-01-14 11:43:23 +1300

    Add C# List to programme

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,8 +8,11 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new [] {12.7, 10.3, 6.11};
+            var grades = new List<double>(){12.7, 10.3, 6.11};
+            grades.Add(56.1);
+            
             var result = 0.0;
-            foreach(double number in numbers)
+            foreach(double number in grades)
             {
                 result += number;
             }
