13050050cb62e03189c58cb5e198d07a2c42bde5
robert-cummins <robert.k.cummins@gmail.com>
2020-01-14 11:50:33 +1300

    Add grade average functionality

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,6 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new [] {12.7, 10.3, 6.11};
             var grades = new List<double>(){12.7, 10.3, 6.11};
             grades.Add(56.1);
             
@@ -16,7 +15,10 @@ namespace GradeBook
             {
                 result += number;
             }
-            Console.WriteLine(result);
+
+            result = result / grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
