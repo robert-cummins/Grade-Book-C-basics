88cf9f75c75111948086c66f769bb0318ac29b25
robert-cummins <robert.k.cummins@gmail.com>
2020-01-20 16:44:49 +1300

    Implement new classes

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    class Book 
+    {
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+
             var grades = new List<double>(){12.7, 10.3, 6.11};
             grades.Add(56.1);
             
@@ -17,7 +23,7 @@ namespace GradeBook
             }
 
             result = result / grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The average grade is {result:N2}");
 
             if(args.Length > 0)
             {
