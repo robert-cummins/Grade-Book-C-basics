aacd455b3ee6b6a00d18cf17757296105403c58c
robert-cummins <robert.k.cummins@gmail.com>
2020-01-21 11:20:32 +1300

    Add new book class method in seperate file

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace  GradeBook
+{
+    class Book
+    {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        List<double> grades;
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book 
-    {
-        
-    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            
 
-            var grades = new List<double>(){12.7, 10.3, 6.11};
+            var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
             
             var result = 0.0;
@@ -23,16 +23,8 @@ namespace GradeBook
             }
 
             result = result / grades.Count;
-            Console.WriteLine($"The average grade is {result:N2}");
+            Console.WriteLine($"The average grade is {result:N1}");
 
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
