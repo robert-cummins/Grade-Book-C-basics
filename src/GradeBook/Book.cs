fa4811fbc916c7e8aceb1d443f38b1771f427a82
robert-cummins <robert.k.cummins@gmail.com>
2020-01-21 12:12:34 +1300

    Add show statistics method to book

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace  GradeBook
@@ -13,7 +14,26 @@ namespace  GradeBook
         {
             grades.Add(grade);
         }
-        List<double> grades;
+
+        public void showStatistics()
+        {
+            var lowGrade = double.MaxValue;
+            var highGrade = double.MinValue;
+            var result = 0.0;
+
+            foreach(double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result = result / grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The Highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+        }
+        private List<double> grades;
         private string name;
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,26 +12,7 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-             
-
-            var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
-            grades.Add(56.1);
-            
-            var lowGrade = double.MaxValue;
-            var highGrade = double.MinValue;
-            var result = 0.0;
-            foreach(double number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-
-            result = result / grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            Console.WriteLine($"The Highest grade is {highGrade:N1}");
-            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
-
+            book.showStatistics(); 
         }
     }
 }
