3628fc3eadf29e6536e37900d8eb12186f53a916
robert-cummins <robert.k.cummins@gmail.com>
2020-01-28 16:29:44 +1300

    refactored to pass unit tests

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -14,6 +14,13 @@ namespace GradeBook.Tests
             book.AddGrade(90.5);
             book.AddGrade(77.3);
 
+            //act
+           var result = book.GetStatistics();
+
+           //assert
+           Assert.Equal(85.6, result.Average, 1);
+           Assert.Equal(90.5, result.High, 1);
+           Assert.Equal(77.3, result.Low, 1);
         }
     }
 }
