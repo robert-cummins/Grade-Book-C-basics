71527310fb237451f6d2e0b5a68a00621d58e3f5
robert-cummins <robert.k.cummins@gmail.com>
2020-01-28 14:45:43 +1300

    Change unit test name to BookTest

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -3,11 +3,16 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class UnitTest1
+    public class BookTests
     {
         [Fact]
         public void Test1()
         {
+            var x = 5;
+            var y = 2;
+            var expected = 7;
+            var actual = x + y;
+            Assert.Equal(expected, actual);
 
         }
     }
