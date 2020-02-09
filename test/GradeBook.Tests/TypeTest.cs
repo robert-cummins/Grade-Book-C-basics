89038f670efc3a5d2d9437985cdda88d27315004
robert-cummins <robert.k.cummins@gmail.com>
2020-02-10 11:13:26 +1300

    Add more C# type tests

--- test/GradeBook.Tests/TypeTest.cs
+++ test/GradeBook.Tests/TypeTest.cs
@@ -6,6 +6,36 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CSharpIsPassedByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+           Assert.Equal("Book 1", book1.Name);
+           
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+           Assert.Equal("New Name", book1.Name);
+           
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
