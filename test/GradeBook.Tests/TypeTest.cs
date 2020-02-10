11b7f27481e0d26e7bdda88fa9c4fa004b27a411
robert-cummins <robert.k.cummins@gmail.com>
2020-02-10 16:49:24 +1300

    Add test to show that strings are immutable

--- test/GradeBook.Tests/TypeTest.cs
+++ test/GradeBook.Tests/TypeTest.cs
@@ -5,13 +5,47 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-         [Fact]
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "scott";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("scott", name);
+            Assert.Equal("SCOTT", upper);
+        }
+
+        private string MakeUpperCase(string parameter)
+        {
+           return parameter.ToUpper();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
         public void CSharpCanPassByRef()
         {
             var book1 = GetBook("Book 1");
             GetBookSetName(ref book1, "New Name");
 
-           Assert.Equal("New Name", book1.Name);
+            Assert.Equal("New Name", book1.Name);
            
         }
 
@@ -27,7 +61,7 @@ namespace GradeBook.Tests
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
 
-           Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 1", book1.Name);
            
         }
 
@@ -42,7 +76,7 @@ namespace GradeBook.Tests
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name");
 
-           Assert.Equal("New Name", book1.Name);
+            Assert.Equal("New Name", book1.Name);
            
         }
 
@@ -57,8 +91,8 @@ namespace GradeBook.Tests
             var book1 = GetBook("Book 1");
             var book2 = GetBook("Book 2");
 
-           Assert.Equal("Book 1", book1.Name);
-           Assert.Equal("Book 2", book2.Name);
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
         }
 
         [Fact]
@@ -67,8 +101,8 @@ namespace GradeBook.Tests
             var book1 = GetBook("Book 1");
             var book2 = book1;
 
-           Assert.Same(book1, book2);
-           Assert.True(Object.ReferenceEquals(book1, book2));
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
         Book GetBook(string name)
